efd64a85ea0a16b3a2bd368f17b1afdd15abbbb5
Ying <Ying.XIAO@outpayce.com>
2024-02-20 10:00:16 +0100

    Complete init indexedDB and search from indexedDB.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -14,52 +14,58 @@ const keyList = [
 const mapPositionToKey = (position: number) => keyList[position];
 
 // the setup of IndexDB, and the methods to use it.
-export function initializeDB(words: string[]) {
+export function initializeDB(words: string[]): Promise<void> {
   // add wordObject to the database
-  const request = indexedDB.open(dbName, 1);
+  const request = indexedDB.open(dbName, 2);
 
-  request.onerror = (event) => {
-    // Handle errors.
-  };
-
-  request.onupgradeneeded = (event) => {
-    const db = (event.target as IDBOpenDBRequest)?.result;
-
-    if (!db) return;
-
-    // Create an objectStore to hold information about the words.
-    const objectStore = db.createObjectStore('words', { autoIncrement: true });
-
-    // Create an index to search words by first letter.
-    objectStore.createIndex('firstLetter', 'firstLetter', { unique: false });
-
-    // Create an index to search words by second letter.
-    objectStore.createIndex('secondLetter', 'secondLetter', { unique: false });
-
-    // Create an index to search words by third letter.
-    objectStore.createIndex('thirdLetter', 'thirdLetter', { unique: false });
-
-    // Create an index to search words by fourth letter.
-    objectStore.createIndex('fourthLetter', 'fourthLetter', { unique: false });
-
-    // Create an index to search words by fifth letter.
-    objectStore.createIndex('fifthLetter', 'fifthLetter', { unique: false });
-
-    // Create an index to search words by sixth letter.
-    objectStore.createIndex('sixthLetter', 'sixthLetter', { unique: false });
-
-    // Create an index to search words by seventh letter.
-    objectStore.createIndex('seventhLetter', 'seventhLetter', { unique: false });
-
-    // Store values in the newly created objectStore.
-    objectStore.transaction.oncomplete = (event) => {
-      const wordObjectStore = db.transaction('words', 'readwrite').objectStore('words');
-      words.forEach((word) => {
-        const wordObject = createWordObject(word);
-        if (wordObject) wordObjectStore.add(wordObject);
-      });
+  return new Promise((resolve, reject) => {
+    request.onerror = (event) => {
+      // Handle errors.
+      reject(event);
     };
-  };
+
+    request.onupgradeneeded = (event) => {
+      const db = (event.target as IDBOpenDBRequest)?.result;
+
+      if (!db) return;
+
+      // Create an objectStore to hold information about the words.
+      const objectStore = db.createObjectStore('words', { autoIncrement: true });
+
+      // Create an index to search words by first letter.
+      objectStore.createIndex('firstLetter', 'firstLetter', { unique: false });
+
+      // Create an index to search words by second letter.
+      objectStore.createIndex('secondLetter', 'secondLetter', { unique: false });
+
+      // Create an index to search words by third letter.
+      objectStore.createIndex('thirdLetter', 'thirdLetter', { unique: false });
+
+      // Create an index to search words by fourth letter.
+      objectStore.createIndex('fourthLetter', 'fourthLetter', { unique: false });
+
+      // Create an index to search words by fifth letter.
+      objectStore.createIndex('fifthLetter', 'fifthLetter', { unique: false });
+
+      // Create an index to search words by sixth letter.
+      objectStore.createIndex('sixthLetter', 'sixthLetter', { unique: false });
+
+      // Create an index to search words by seventh letter.
+      objectStore.createIndex('seventhLetter', 'seventhLetter', { unique: false });
+
+      // Store values in the newly created objectStore.
+      objectStore.transaction.oncomplete = () => {
+        const wordObjectStore = db.transaction('words', 'readwrite').objectStore('words');
+        words.forEach((word) => {
+          const wordObject = createWordObject(word);
+          if (wordObject) wordObjectStore.add(wordObject);
+        });
+      };
+    };
+    request.onsuccess = () => {
+      resolve();
+    };
+  });
 }
 
 export function isValidWord(word: string): boolean {
@@ -69,35 +75,37 @@ export function isValidWord(word: string): boolean {
 export function searchInDB(searchParams: {
   maxLength?: number;
   searchLetters: { letter: Letter; position: number }[];
-}): string[] {
-  let key = mapPositionToKey(searchParams.searchLetters[0].position);
+}): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    const key = mapPositionToKey(searchParams.searchLetters[0].position);
 
-  const request = indexedDB.open(dbName, 1);
-  request.onerror = (event) => {
-    // Handle errors.
-  };
-
-  request.onsuccess = (event) => {
-    let db = (event.target as IDBOpenDBRequest)?.result;
-
-    if (!db) return [];
-
-    const wordObjectStore = db.transaction('words', 'readonly').objectStore('words');
-    const index = wordObjectStore.index(key);
-    let range = IDBKeyRange.only(searchParams.searchLetters[0].letter);
-
-    let cursorRequest = index.openCursor(range);
-
-    cursorRequest.onsuccess = (event) => {
-      let cursor = (event.target as IDBRequest)?.result;
-
-      if (cursor) {
-        console.log(cursor.value);
-        cursor.continue();
-      }
+    const request = indexedDB.open(dbName, 2);
+    request.onerror = (event) => {
+      reject(event);
     };
-  };
-  return [];
+
+    request.onsuccess = (event) => {
+      const db = (event.target as IDBOpenDBRequest)?.result;
+
+      if (!db) return [];
+
+      const wordObjectStore = db.transaction('words', 'readonly').objectStore('words');
+      const index = wordObjectStore.index(key);
+      const indexRequest = index.getAll(searchParams.searchLetters[0].letter);
+
+      indexRequest.onsuccess = (event) => {
+        const wordObjects = (event.target as IDBRequest)?.result;
+        resolve(
+          wordObjects.map((wordObject: { [x: string]: string }) =>
+            keyList
+              .map((key) => wordObject[key])
+              .filter((val) => val)
+              .join('')
+          )
+        );
+      };
+    };
+  });
 }
 
 export function createWordObject(word: string): any {
@@ -106,9 +114,7 @@ export function createWordObject(word: string): any {
     for (let i = 0; i < word.length; i++) {
       wordObject[keyList[i]] = word[i];
     }
-
     return wordObject;
   }
-
   return null;
 }
